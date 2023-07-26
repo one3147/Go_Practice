fb2274971b2788bf49f9b9224c2426c71e0f4ed5
Wonjune <103029974+one3147@users.noreply.github.com>
2023-07-26 09:24:27 +0900

    Create map.go

---
+++ practice/map.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	f1 := []string{"사과", "바나나", "토마토"}
+	f2 := []string{"포도", "딸기"}
+	f3 := append(f1, f2...)
+	f4 := append(f1[:2], f2...)
+	fmt.Println(f1)
+	fmt.Println(f2)
+	fmt.Println(f3)
+	fmt.Println(f4)
+}
